304bba5ad4154a2ef01fb0b04dbb824b598f49ad
Alvaro Oliveira <alvaro.oliveira@compasso.com.br>
2021-08-25 11:41:33 -0300

    First Commit

---
+++ src/main/java/br/com/mydrafts/ApiMyDrafts/ApiMyDraftsApplication.java
@@ -0,0 +1,13 @@
+package br.com.mydrafts.ApiMyDrafts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMyDraftsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMyDraftsApplication.class, args);
+	}
+
+}
