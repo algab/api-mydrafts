11fa771da3590fd0409f40607518b34466249c0f
Alvaro Oliveira <alvaro.oliveira@compasso.com.br>
2021-08-25 14:54:12 -0300

    add openfeign

--- src/main/java/br/com/mydrafts/ApiMyDrafts/ApiMyDraftsApplication.java
+++ src/main/java/br/com/mydrafts/ApiMyDrafts/ApiMyDraftsApplication.java
@@ -2,8 +2,10 @@ package br.com.mydrafts.ApiMyDrafts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ApiMyDraftsApplication {
 
 	public static void main(String[] args) {
