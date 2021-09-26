b515413e7325e2632f0e1a4af018cd083425de47
Alvaro Oliveira <algab.oliveira@gmail.com>
2021-09-26 18:35:10 -0300

    add method existsByEmail UserRepository

--- src/main/java/br/com/mydrafts/ApiMyDrafts/repository/UserRepository.java
+++ src/main/java/br/com/mydrafts/ApiMyDrafts/repository/UserRepository.java
@@ -3,4 +3,8 @@ package br.com.mydrafts.ApiMyDrafts.repository;
 import br.com.mydrafts.ApiMyDrafts.documents.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, String> {}
+public interface UserRepository extends MongoRepository<User, String> {
+
+    Boolean existsByEmail(String email);
+
+}
